a2d2933764406c998b12aa85fe4302bf6bbdb873
inksea <oebudde13@gmail.com>
2020-07-07 21:07:02 -0400

    Working on ToDo list, starting to generate items in js

--- todo/todo.js
+++ todo/todo.js
@@ -1,50 +1,103 @@
 console.log('In todo.js!');
 
+// Init.
+const todoState = [];
+
 // Selectors.
-const todoInput = document.getElementById('todoInput');
-const addTodoButton = document.querySelector('#addTodoButton');
-const todoList = document.querySelector('#todoList');
-const todos = ['Static Test Todo'];
+const addInput = document.querySelector('.add-control .form-group input.form-control');
+const addInputButton = document.querySelector('.add-control .form-group i.form-control-feedback');
+const errParagraph = document.querySelector('.err');
+const todoListElement = document.querySelector('.todo-list');
+// console.log(addInputButton);
 
-// JS addEventListener Method: Add an event listener to an html element that has been
-// selected in js
-// Anon Function: Function is not defined elsewhere in the codebase, and is called
-// immediately within the arg.
-addTodoButton.addEventListener('click', function() {
-    // Get the todoInput value.
-    let todoValue = todoInput.value;
+// Bind click event to plus button on input field, and fire addTodo function.
+addInputButton.addEventListener('click', addTodo);
 
-    // Sanitize | trim = removing trailing and preceding whitespace.
-    todoValue = todoValue.trim();
+// document.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     // Reset Error messages.
+//     errParagraph.classList.add('hidden');
+// })
 
-    // Push the todo input value to the todos array.
-    if (todoValue !== '') {
-        todos.push(todoValue);
+// Add Todo
+function addTodo(event) {
+    // Reset Error message.
+    errParagraph.classList.add('hidden');
+
+    // Take in the input value
+    const todoValue = addInput.value
+
+    // Check if todoValue has an actual value.
+    if (todoValue) {
+        todoState.push(todoValue);
+
+        // Generate a todo item.
+        const newTodo = generateTodoItem(todoValue);
+        todoListElement.appendChild(newTodo)
+
+
+        // Reset the value to empty string.
+        addInput.value = '';
+
+
+        // Append that item to a list or div
     } else {
-        // Modify the input element to have border red.
-        todoInput.style.border = '1px solid red';
-        // alert('You can\'t submit an empty value.');
+        // Handle error.
+        errParagraph.classList.remove('hidden');
     }
-});
 
-// Generate todo (<li>) element
-// function generateTodo(todo) {
-//     const task = document.createElement('li');
-//     const taskText = document.createElement('span');
-//     const checkbox = document.createElement('input');
-//     const deleteButton = document.createElement('button');
+    console.log(todoState);
+}
 
-//     // Checkbox.
-//     checkbox.setAttribute('type', 'checkbox');
+function generateTodoItem(value) {
+    // Create our html elements.
+    const todoItem = document.createElement('li');
+    const todoItemCheckbox = document.createElement('input');
+    const todoItemLabel = document.createElement('label');
+    // const todoItemSpan = document.createElement('span');
 
-//     // Insert text
-//     taskText.innerText = todo;
+    // Checkbox
+    todoItemCheckbox.setAttribute('type', 'checkbox');
 
-//     // Build the task item.
-//     task.appendChild(checkbox);
-//     task.appendChild(taskText);
+    // Label
+    todoItemLabel.innerText = value;
 
-//     return task;
+    // Add styles
+    todoItem.appendChild(todoItemCheckbox);
+    todoItem.appendChild(todoItemLabel);
+
+    return todoItem;
+
+}
+
+
+
+
+
+
+// ============================================
+// LECTURE TEST
+// Triangle Loop
+// let hashes = '';
+// for (let index = 0; index <= 7; index++) {
+//     hashes += '#';
+//     console.log(hashes);
 // }
 
-// todoList.appendChild(generateTodo('__test__'))
+// Remember: break the solution down into its simplest parts
+// Part 1: limit to seven lines
+// Part 2. Create an initial value
+
+// FizzBuzz
+// Use Mod %
+// for (let index = 1; index <= 100; index++) {
+//     if (index % 3 === 0 && index % 5 === 0) {
+//         console.log('FizzBuzz');
+//     } else if (index % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (index % 5 === 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(index);
+//     }
+// }
